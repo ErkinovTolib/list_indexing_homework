40f6790ab3db284a3a63129b2f641f64108b5923
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-01 22:38:34 +0500

    List09

--- List09.py
+++ List09.py
@@ -1,6 +1,7 @@
 def main(list1):
     """
-    A list of several elements is given. True if all items are the same, otherwise return False.
+    A list of several elements is given. True if all items are the same,
+     otherwise return False.
     Args:
         list1 (list): parameter
     Returns:
@@ -8,8 +9,9 @@ def main(list1):
     """
     i = 0
     while i<len(list1):
-        if list1[i]==0:
-            list[i]==True
+        if list1[0] == list1[i]:
+            return True
+        list1[0] != list1[i]
         i += 1
-    return list1
-print(main([0,0,0,0,0]))
+    return False
+print(main([1,1,1,1,1]))
