0589dddb97bde1a5c5e3f2e2a7e3e0ad2ed69995
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-01 18:37:56 +0500

    List08

--- List08.py
+++ List08.py
@@ -1,9 +1,19 @@
 def main(list1):
     """
-    A list of ones and zeros, five in length, is given. replace one with True, replace zeros with False.
+    A list of ones and zeros, five in length, is given. 
+    replace one with True, replace zeros with False.
     Args:
         list1 (list): parameter
     Returns:
         list: return answer
     """
-    return
+    i = 0
+    i = 0
+    while i < len(list1):
+        if(list1[i] == 0):
+            list1[i] = False
+        elif list1[i]==1:
+            list1[i]==True
+        i += 1
+    return list1
+print(main([1,0,0,0,0]))
