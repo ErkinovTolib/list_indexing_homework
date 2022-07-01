ca8f871e817f1dab788421927f4b85dc4c165bfd
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-01 22:25:39 +0500

    List10

--- List10.py
+++ List10.py
@@ -6,4 +6,13 @@ def main(list_num):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    while i<len(list_num):
+        if list_num[i]>list_num[i-1]:
+            c = list_num[i]
+        else:
+            c = list_num[i-1]
+        i += 1
+    return c
+print(main([1,8,9,5]))
+        
