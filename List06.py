b1468269a8de4f68f14ed0bc18e16fa2e19bb398
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-01 18:31:42 +0500

    List06

--- List06.py
+++ List06.py
@@ -1,9 +1,17 @@
 def main(list1):
     """
-    A list of units and zeros with a length of five is given. Replace one with True.
+    A list of units and zeros with a length of five is given. 
+    Replace one with True.
     Args:
         list1 (list): parameter
     Returns:
         list: return answer
     """
-    return
+    i = 0
+    while i < len(list1):
+        if list1[i] == 1:
+            list1[i] = True
+        i += 1
+    return list1
+print(main([1,0,0,0,0]))
+    
