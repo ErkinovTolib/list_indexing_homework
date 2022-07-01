e2a061c842870b05b0ef6be27ae6f27f0490c59e
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-01 23:23:46 +0500

    List09

--- List09.py
+++ List09.py
@@ -1,6 +1,3 @@
-from multiprocessing.connection import answer_challenge
-
-
 def main(list1):
     """
     A list of several elements is given. True if all items are the same,
@@ -10,13 +7,9 @@ def main(list1):
     Returns:
         bool: return answer
     """
-    i = 0
-    ans = 0
-    while i<len(list1):
-        if list1[0] == list1[i]:
-            ans = True
-        else:
-            ans = False
-        i += 1
-    return ans
-print(main([0,0,0,0,1]))
+    n = list1.count(list1[0])
+    if  n == len(list1):
+        return True
+    else:
+        return False
+print(main([1,1,1,1]))
